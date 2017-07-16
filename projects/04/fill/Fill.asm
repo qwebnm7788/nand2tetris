bdd134aa1a34f5880256b790f975f11e5cc4e997
Jaewon Choi <qwebnm7788@naver.com>
2017-07-16 21:27:45 +0900

    week4 update

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -12,3 +12,132 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+
+
+(INFLOOP)
+	//2중 for루프의 i,j 변수 초기화	
+	@i
+	M = 0
+	@j
+	M = 0
+
+	//내부 for루프는 32회 (512/16)
+	@32
+	D = A
+	@inner
+	M = D
+
+	//외부 for루프는 256회
+	@256
+	D = A
+	@outer
+	M = D
+
+	//SCREEN영역의 주소로 초기화
+	@SCREEN
+	D = A
+	@addr
+	M = D
+
+	//키보드 입력 확인 (아무 입력이 없다면 0이므로 WHITELOOP1로 이동)
+	@KBD
+	D = M
+	@WHITELOOP1
+	D;JEQ
+
+	(BLACKLOOP1)
+		//j = 0
+		@j
+		M = 0
+		(BLACKLOOP2)
+			//현재 위치에 -1 저장
+			@addr
+			A = M
+			M = -1
+
+			//주소 한칸 이동
+			@addr
+			M = M + 1
+
+			//j++
+			@j
+			M = M + 1
+
+			//j - inner를 D에 저장
+			@inner
+			D = M
+			@j
+			D = M - D
+
+			//j-inner가 0이라면 내부 for루프 종료
+			@BLACKEND2
+			D; JEQ
+
+			@BLACKLOOP2
+			0; JMP
+		(BLACKEND2)
+		//i++
+		@i
+		M = M + 1
+
+		//i - outer를 D에 저장
+		@outer
+		D = M
+		@i
+		D = M - D
+
+		//i-outer = 0이면 외부 for루프 종료
+		@BLACKEND1
+		D; JEQ
+
+		@BLACKLOOP1
+		0; JMP
+	(BLACKEND1)
+
+	@DONE
+	0; JMP
+
+	(WHITELOOP1)
+		@j
+		M = 0
+		(WHITELOOP2)
+			@addr
+			A = M
+			M = 0			//이번엔 0으로 clear
+
+			@addr
+			M = M + 1
+
+			@j
+			M = M + 1
+
+			@inner
+			D = M
+			@j
+			D = M - D
+
+			@WHITEEND2
+			D; JEQ
+
+			@WHITELOOP2
+			0; JMP
+		(WHITEEND2)
+		@i
+		M = M + 1
+
+		@outer
+		D = M
+		@i
+		D = M - D
+
+		@WHITEEND1
+		D; JEQ
+
+		@WHITELOOP1
+		0; JMP
+	(WHITEEND1)
+	
+	(DONE)
+	@INFLOOP
+	0;JMP
+	
--- projects/04/mult/mult.asm
+++ projects/04/mult/mult.asm
@@ -7,3 +7,42 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+
+@1
+D = M
+//반복문 횟수 i = RAM[1]
+@i
+M = D
+//결과값 sum = 0으로 초기화
+@sum
+M = 0
+
+(LOOP)
+	@i
+	D = M
+	//i == 0 ? 조건검사
+	@END
+	D;JEQ
+
+	@0
+	D = M
+	//sum += RAM[0]
+	@sum
+	M = M + D
+	//i--
+	@i
+	M = M - 1
+
+	@LOOP
+	0;JMP
+
+(END)
+
+@sum
+D = M
+//RAM[2] = sum
+@2
+M = D
+//종료 무한루프
+@22
+0;JMP
